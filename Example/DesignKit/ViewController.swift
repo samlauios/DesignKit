e386e1bfd3aac7fab70f1d175534f9f0516f17eb
Sam Lau <samlau@YSB-mac-mini.local>
2021-11-30 14:55:06 +0800

    Initial commit

---
+++ Example/DesignKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DesignKit
+//
+//  Created by samlauios on 11/30/2021.
+//  Copyright (c) 2021 samlauios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
